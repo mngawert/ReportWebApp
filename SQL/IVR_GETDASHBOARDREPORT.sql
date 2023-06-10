97cce878ddc01d54ff7d0b4cc9bedb60952441d2
montrin <Convergys131>
2023-06-10 10:25:56 +0700

    Add IVR dashboard sql file.

---
+++ SQL/IVR_GETDASHBOARDREPORT.sql
@@ -0,0 +1,6 @@
+﻿select CALLED_PARTY as DestinationAddress, count(1) as TotalCount
+from CALL_IVR_CC_[ID] a
+where date_format(a.CALL_START_TIME, '%Y') = {0}
+and (date_format(a.CALL_START_TIME, '%m') = {1} OR {1} = '')
+and 'Success' = {2}
+group by CALLED_PARTY
