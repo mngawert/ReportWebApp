c88d51ff29f9d0477f642460a003c4c44de6a779
montrin <Convergys131>
2024-03-02 23:40:00 +0700

    Update MCN query to use CALL_IVR_CC_ table with service_name = MCA

--- SQL/MCN_GETDASHBOARDREPORT.sql
+++ SQL/MCN_GETDASHBOARDREPORT.sql
@@ -1,6 +1,7 @@
-﻿select DESTINATING_ADDRESS as DestinationAddress, count(1) as TotalCount
-from MCA_VMS_CC_[ID] a
-where date_format(a.CALL_TIMESTAMP, '%Y') = {0}
-and (date_format(a.CALL_TIMESTAMP, '%m') = {1} OR {1} = '')
+﻿select CALLED_PARTY as DestinationAddress, count(1) as TotalCount
+from CALL_IVR_CC_[ID] a
+where date_format(a.CALL_START_TIME, '%Y') = {0}
+and (date_format(a.CALL_START_TIME, '%m') = {1} OR {1} = '')
 and 'Success' = {2}
-group by destination_address
+and service_name = 'MCA'
+group by CALLED_PARTY
--- SQL/MCN_GETREPORT.sql
+++ SQL/MCN_GETREPORT.sql
@@ -1,10 +1,10 @@
 ﻿SELECT  TRANSACTION_ID as TransactionId,
-        IF(CALL_TIMESTAMP='0000-00-00 00:00:00.000',NULL,CALL_TIMESTAMP) as DeliveryTime,
-        date_format(CALL_TIMESTAMP, '%d %M %Y %T') as DeliveryTimeText,
-        ORIGINATING_ADDRESS as OriginationAddress,
-        DESTINATING_ADDRESS as DestinationAddress, 
-        /*IF(Message_Status=255, 1, 2) as MessageStatus,*/
-        1 as MessageStatus,
-        STATUS as InternalMessageStatus,
-        Message_Type as MessageType
-FROM MCA_VMS_CC_[ID] a
+    IF(CALL_START_TIME='0000-00-00 00:00:00.000',NULL,CALL_START_TIME) as DeliveryTime,
+    date_format(CALL_START_TIME, '%d %M %Y %T') as DeliveryTimeText,
+    CALLING_PARTY as OriginationAddress,
+    CALLED_PARTY as DestinationAddress, 
+    1 as MessageStatus,
+    STATUS_CODE as InternalMessageStatus,
+    NULL as MessageType
+FROM CALL_IVR_CC_[ID] a
+where service_name = 'MCA'
